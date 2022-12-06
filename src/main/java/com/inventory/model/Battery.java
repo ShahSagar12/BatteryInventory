4633773a416df40a814ab482a46bfee018b7607b
SagarShahc2 <sagar@c2.my>
2022-12-06 21:36:40 +0545

    project modeled

--- src/main/java/com/inventory/model/Battery.java
+++ src/main/java/com/inventory/model/Battery.java
@@ -1,5 +1,13 @@
 package com.inventory.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * This class is a part of the package com.inventory.model and the package
  * is a part of the project BatteryInventory.
@@ -9,9 +17,20 @@ package com.inventory.model;
  * <p>
  * Created by sagar on 2022-12-06.
  */
+@Entity
+@NoArgsConstructor
+@Data
 public class Battery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
     private String name;
     private String postcode;
     private String capacity;
+
+    public Battery(String name, String postcode, String capacity) {
+        this.name = name;
+        this.postcode = postcode;
+        this.capacity = capacity;
+    }
 }
