d7adfd7be07e8c9aeeb4e49cec128bfbad8b21f1
SagarShahc2 <sagar@c2.my>
2022-12-06 21:04:21 +0545

    added lombok dependency

---
+++ src/main/java/com/inventory/model/Battery.java
@@ -0,0 +1,17 @@
+package com.inventory.model;
+
+/**
+ * This class is a part of the package com.inventory.model and the package
+ * is a part of the project BatteryInventory.
+ * <p>
+ * Connecting Creations Pvt. Ltd. Lalitpur, Nepal.
+ * https://c2.my/
+ * <p>
+ * Created by sagar on 2022-12-06.
+ */
+public class Battery {
+    private String id;
+    private String name;
+    private String postcode;
+    private String capacity;
+}
